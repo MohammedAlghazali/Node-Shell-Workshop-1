250c1d1de116dae3d4b42659348e02be1dc4aaa4
mohammedAlghazali <hamood-monzer11@hotmail.com>
2020-10-07 21:05:44 +0300

    handle case of file not exist(undefined)

--- lesson-1/cat.js
+++ lesson-1/cat.js
@@ -2,7 +2,7 @@ const { readFile } = require('fs');
 
 const fileToRead = process.argv[2];
 
-readFile(fileToRead, (err, data) => {
+readFile(process.cwd() + '/' + fileToRead, (err, data) => {
   if (err) {
     process.stdout.write('file does not exist');
   }
