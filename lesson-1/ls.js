f0a3c5081f2cd455ef3c9f648ed2ac9420c4744f
mohammedAlghazali <hamood-monzer11@hotmail.com>
2020-10-07 21:42:29 +0300

    fix bug in ls.js fix path in case of no argument

--- lesson-1/ls.js
+++ lesson-1/ls.js
@@ -4,6 +4,7 @@ const { readdir } = require('fs');
 
 const fileToRead = process.argv[2];
 if (process.argv[3] === '-ex' && process.argv[4]) {
+  console.log('hello from if');
   readdir(process.cwd() + '/' + fileToRead, (err, files) => {
     if (!err) {
       files.forEach((file) => {
@@ -15,10 +16,10 @@ if (process.argv[3] === '-ex' && process.argv[4]) {
     }
   });
 } else {
-  readdir(process.cwd() + '/' + fileToRead, (err, files) => {
+  readdir(process.cwd(), (err, files) => {
     if (!err) {
       files.forEach((file) => {
-        process.stdout.write(file + ' ');
+        process.stdout.write(file + '\n');
       });
     }
   });
